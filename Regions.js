7da966f6893a017814460ae4554477547466dcb2
Furkan Uyanık <51247701+furkanuyanik@users.noreply.github.com>
2019-12-16 12:06:44 +0300

    Created regions

---
+++ Regions.js
@@ -0,0 +1,54 @@
+const regions = [
+    { regionName: 'KIBRIS', cities: ['KIBRIS'] },
+    { regionName: 'ANKARA', cities: ['AKSARAY', 'KARAMAN', 'KIRIKKALE', 'KIRSEHIR', 'KONYA', 'ANKARA'] },
+    { regionName: 'BURSA', cities: ['BALIKESIR', 'BILECIK', 'BURSA', 'CANAKKALE', 'ESKISEHIR', 'KUTAHYA', 'YALOVA'] },
+    {
+        regionName: 'SAMSUN',
+        cities: [
+            'AMASYA',
+            'ARTVIN',
+            'BAYBURT',
+            'CORUM',
+            'GIRESUN',
+            'GUMUSHANE',
+            'KASTAMONU',
+            'ORDU',
+            'RIZE',
+            'SAMSUN',
+            'SINOP',
+            'TRABZON'
+        ]
+    },
+    { regionName: 'KAYSERI', cities: ['KAHRAMANMARAS', 'KAYSERI', 'NEVSEHIR', 'NIGDE', 'SIVAS', 'TOKAT', 'YOZGAT'] },
+    { regionName: 'TRAKYA', cities: ['EDIRNE', 'KIRKLARELI', 'TEKIRDAG'] },
+    {
+        regionName: 'DIYARBAKIR',
+        cities: [
+            'ADIYAMAN',
+            'BATMAN',
+            'BITLIS',
+            'DIYARBAKIR',
+            'ELAZIG',
+            'HAKKARI',
+            'MALATYA',
+            'MARDIN',
+            'SANLIURFA',
+            'SIIRT',
+            'SIRNAK',
+            'TUNCELI',
+            'VAN'
+        ]
+    },
+    { regionName: 'IZMIR', cities: ['AYDIN', 'DENIZLI', 'IZMIR', 'MANISA', 'USAK'] },
+    {
+        regionName: 'SAKARYA',
+        cities: ['BARTIN', 'BOLU', 'CANKIRI', 'DUZCE', 'KARABUK', 'KOCAELI', 'SAKARYA', 'ZONGULDAK']
+    },
+    { regionName: 'ASYA', cities: ['ASYA'] },
+    { regionName: 'AVRUPA', cities: ['AVRUPA'] },
+    { regionName: 'ADANA', cities: ['ADANA', 'GAZIANTEP', 'HATAY', 'MERSIN', 'KILIS', 'OSMANIYE'] },
+    { regionName: 'ANTALYA', cities: ['AFYON', 'ANTALYA', 'BURDUR', 'ISPARTA', 'MUGLA'] },
+    { regionName: 'ERZURUM', cities: ['ERZURUM', 'ERZINCAN', 'AGRI', 'ARDAHAN', 'BINGOL', 'IGDIR', 'KARS', 'MUS'] }
+];
+
+export default regions;
